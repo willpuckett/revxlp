147eb97fc53562a78c852cc089efaf397dfcdc26
Peter Johanson <peter@peterjohanson.com>
2022-12-23 20:30:27 -0500

    feat: Add CNC friendly case generation.

--- plate-case-export.py
+++ plate-case-export.py
@@ -8,7 +8,7 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(prog='plate-case-export')
-parser.add_argument('--feature', '-f', action='append', choices=['base', 'logo', 'button_cutouts'])
+parser.add_argument('--feature', '-f', action='append', choices=['base', 'logo', 'button_cutouts', 'cnc'])
 parser.add_argument('version', choices=['10u', '12u'])
 
 args = parser.parse_args(sys.argv[1:])
@@ -17,11 +17,16 @@ args = parser.parse_args(sys.argv[1:])
 model = cqgi.parse(open("plate-case.py").read())
 
 opts = {
-        'design_size': args.version,
-        'assembly_generation': False,
-        'feature_button_cutouts': (not args.feature or "button_cutouts" in args.feature),
-        'feature_logo': (not args.feature or "logo" in args.feature),
+    'design_size': args.version,
+    'assembly_generation': False,
+    'feature_button_cutouts': (not args.feature or "button_cutouts" in args.feature),
+    'feature_logo': (not args.feature or "logo" in args.feature),
 }
+
+if (args.feature and "cnc" in args.feature):
+    opts['pcb_wiggle_room'] = 0.4
+    opts['feature_fillet_inside'] = False
+    opts['screw_hole_type'] = 'cbore'
 
 # run the script and store the result (from the show_object call in the script)
 build_result = model.build(build_parameters = opts)
